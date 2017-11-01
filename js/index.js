e5032da824b96a1ff76b19a53bc8ccf4398bc849
David Eisenmenger <david.eisenmenger@gmail.com>
2017-11-01 01:22:33 -0400

    Add Nav Hamburger

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
-// external js: masonry.pkgd.js
+//// external js: masonry.pkgd.js
+//
+//$('.grid').masonry({
+//  itemSelector: '.grid-item',
+//  columnWidth: 160
+//});
 
-$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 160
-});
-
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+//<script>
+//function myFunction() {
+//    var x = document.getElementById("myTopnav");
+//    if (x.className === "topnav") {
+//        x.className += " responsive";
+//    } else {
+//        x.className = "topnav";
+//    }
+//}
+//</script>
