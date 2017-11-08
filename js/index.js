a3b9612d93b5632e492712b47a25f8fea9086a94
David Eisenmenger <david.eisenmenger@gmail.com>
2017-11-07 21:25:22 -0500

    Updated hamburger

--- js/index.js
+++ js/index.js
@@ -1,29 +1,12 @@
-//// external js: masonry.pkgd.js
-//
-//$('.grid').masonry({
-//  itemSelector: '.grid-item',
-//  columnWidth: 160
-//});
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-//<script>
-//function myFunction() {
-//    var x = document.getElementById("myTopnav");
-//    if (x.className === "topnav") {
-//        x.className += " responsive";
-//    } else {
-//        x.className = "topnav";
-//    }
-//}
-//</script>
 
 
-//JQUERY SCRIPT FOR HAMBURGER
+
+//JS FOR HAMBURGER
 //console.log('Hello')
 //console.log($(".menu-toggle"))
 
 $('.menu-toggle').on("click", function() {
-  console.log("CLICK")
+//  console.log("CLICK")
 
   $('nav ul').toggleClass('open');
 
