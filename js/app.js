155f558f554f7202cf62699090abf6db70fbf5fa
David Eisenmenger <david.eisenmenger@gmail.com>
2017-11-11 00:54:25 -0500

    :Updated contact form, added modal to Mission

--- js/app.js
+++ js/app.js
@@ -8,57 +8,28 @@ var $confirmEmail = $("#confirm_email");
 //Hide hints
 $("form span").hide();
 
+function validateEmail($email){
+    var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    var valid = emailReg.test(email);
 
-
-
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-  console.log;
-}
-
-function isEmailValid () {
-  return $email.val().length > 8;
-}
-
-function areEmailsMatching () {
-  return $email.val() === $confirmEmail.val(); 
-}
-
-function canSubmit() {
-  return isEmailValid() && areEmailsMatching();
-}
-
-function emailEvent() {
-  //Find out if email is valid
-  if($email.val().length > 8) {
-     //Hide hint if match
-    $email.next().hide();  
-  } else {
-    //else show hint
-    $email.next().show();
-  }
+    if(!valid) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 function confirmEmailEvent() {
-  //Find out if email and confirmation match
-  if(areEmmailsMatching()) {
-      //Hide hint if match
-    $confirmEmail.next().hide();
-  } else {
-      //Else show hint
-    $confirmEmail.next().show();
-  }
+    //find out if password and confirmation match
+    if($email.val() === $confirmEmail.val()) {
+    //hide hint if matched
+        $confirmEmail.next().hide();
+    } else {
+   //else show hint  
+        $confirmEmail.next().show();
+    }
 }
+ //when event happens on submit
+$email.focus(emailEvent).keyup(emailEvent).focus(confirmEmailEvent).keyup(confirmEmailEvent);
 
-function enableSubmitEvent() {
-  $("#submit").prop("disabled", !canSubmit ())
-}
-
-//When event happens on email input
-$email.focus(emailEvent).keyup(emailEvent).keyup(confirmEmailEvent).keyup(enableSubmitEvent);
-   
-//When event happens on confirmation input
-$confirmEmail.focus(confirmEmailEvent).keyup(confirmEmailEvent).keyup(enableSubmitEvent);
-
- enableSubmitEvent();
+ //when event happenson confirmation input
