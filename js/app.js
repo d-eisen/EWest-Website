73c4250163b19803df6d197b11d54759b60cd6f9
David Eisenmenger <david.eisenmenger@gmail.com>
2018-03-08 19:13:14 -0500

    new contact form

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
+// Plan
+// Enable to the form to CREATE a new record via submit function and POST to database 
+// The submit form currently uses this method
+// Use REGEX to validate email
+// Using the GET method, include a URL to call all records
+// Allow inputs from the form to update the list via PUT (?)
+// Include button to allow user to DELETE the entire list
+
+
+
+
 //===================================
 //Validate Email address with RegEx
 //===================================
 
 function validateEmail($email){
-    var emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-    var valid = emailReg.test(email);
+    let emailReg = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    let valid = emailReg.test(email);
 
     if(!valid) {
         return false;
