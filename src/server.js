f38111fe6b7096d9d63e490335fd86580f44bca0
David Eisenmenger <david.eisenmenger@gmail.com>
2018-03-25 22:41:47 -0400

    Loaded npm packages.

---
+++ src/server.js
@@ -0,0 +1,8 @@
+// Load packages
+const bodyParser = require('body-parser');
+const bootstrap = require('bootstrap');
+const config = require('./config');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const router = require('./routes');
