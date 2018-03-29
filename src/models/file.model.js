15f3499de46a748f0ecabda318f071c779c51899
David Eisenmenger <david.eisenmenger@gmail.com>
2018-03-29 00:04:28 -0400

    Confirm delete in file schema.

--- src/models/file.model.js
+++ src/models/file.model.js
@@ -1,27 +1,44 @@
 const mongoose = require('mongoose');
+const validator = require("email-validator");
+
 
 const FileSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: true,
-        trim: true,
-      },
+      type: String,
+      required: true,
+      trim: true,
+    },
     
-      email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-      },
-    
-      confirmEmail: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-      },
-    });
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
   
+    confirmEmail: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    
+    deleted: {
+      type: Boolean, 
+      default: false},
+    });
+
+// validator.validate('email');
+
+// FileSchema.pre('save', function(validateEmail) {
+//   if(this.email === true) {
+//       next(new Error('Email is invalid'));
+//       return;
+//     }
+
+//   // next();
+// });
+
   const File = mongoose.model('File', FileSchema);
   
   File.count({}, function(err, count) {
